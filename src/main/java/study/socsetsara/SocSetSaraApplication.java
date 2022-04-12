190e7e7948102f0f9861000e30762cc1ef05e4b9
DonToli <strt87@yandex.ru>
2022-04-12 09:13:28 +0300

    Создал MessageController, аннотированный как @RestController, содержащий методы для обработки запросов: на чтение списка сообщений, получение одного сообщения, создание и модификация существующего и удаление.

---
+++ src/main/java/study/socsetsara/SocSetSaraApplication.java
@@ -0,0 +1,13 @@
+package study.socsetsara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocSetSaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocSetSaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/socsetsara/SocSetSaraApplicationTests.java
@@ -0,0 +1,13 @@
+package study.socsetsara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocSetSaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
